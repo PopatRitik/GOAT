8f3535565683175e5e62868d14088d3fae969a14
PopatRitik <popatritik2012@gmail.com>
2024-07-10 12:26:33 +0530

    bg

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,12 +5,23 @@ import './index.css'
 import { ChakraProvider } from "@chakra-ui/react";
 import { BrowserRouter } from "react-router-dom";
 import { RecoilRoot } from 'recoil';
+import { extendTheme } from "@chakra-ui/theme-utils";
+
+const styles = {
+  global: {
+    body: {
+      bgGradient: 'linear(to-br, navy, blue.900, indigo.900)'
+    },
+  },
+};
+
+const theme = extendTheme({ styles });
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <RecoilRoot>
       <BrowserRouter>
-        <ChakraProvider>
+        <ChakraProvider theme={theme}>
           <App />
         </ChakraProvider>
       </BrowserRouter>
